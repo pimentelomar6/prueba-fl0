7870b76426d99225e316ccc3c3ca90db48c80ca2
Omar Pimentel <pimentelomar6@gmail.com>
2024-01-28 19:42:05 -0400

    options

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.Collections;
+
+@SpringBootApplication(proxyBeanMethods = false)
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		String port = System.getenv("PORT");
+		app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
+		app.run(args);
 	}
 
 }
